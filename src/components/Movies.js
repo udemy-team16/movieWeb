eac2c07dc83357ae58bbd25e34dc1cc38e5d5a2d
김영욱 <duddnr787@naver.com>
2023-07-01 00:07:39 +0900

    페이지 네이션 초기

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -10,10 +10,21 @@ import useLikeHook from 'hooks/useLikeHook';
 
 const Movies = () => {
   //custom hook
-  const { movieList, loading, error } = useFetchMovies('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year');
+  const { movieList, loading, error } = useFetchMovies('https://yts.mx/api/v2/list_movies.json?limit=50&minimum_rating=8.8&sort_by=year');
   const { likeList, unlike } = useLikeHook(JSON.parse(localStorage.getItem('likeList')) || []);
-
   const navigate = useNavigate();
+  // pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+  const pageCount = Math.ceil(movieList.length / itemsPerPage);
+
+  //current page movieList
+  const currentMovies = movieList.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  // page Change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   if (error) {
     console.log(error);
@@ -30,7 +41,7 @@ const Movies = () => {
     <div>
       {loading ? <Loading /> :
         <div className={styles.movieWrap}>
-          {movieList.map(movie => {
+          {currentMovies.map(movie => {
             return (
               <div key={movie.id} className={styles.movieImg} style={{ backgroundImage: "url(" + `${movie.medium_cover_image}` + ")" }} >
                 <div className={styles.overlay}>
@@ -39,7 +50,7 @@ const Movies = () => {
                     <p>{movie.rating} / 10</p>
                   </div>
                   <div className={styles.genre}>
-                    <p>{movie.genres}</p>
+                    {movie.genres}
                   </div>
                   <button className={styles.btn} onClick={() => movieDetailHandle(movie.id)}>View Detail</button>
                   <FontAwesomeIcon icon={faHeart} onClick={() => handleColor(movie.id)} style={{ color: likeList.includes(movie.id) ? 'red' : 'black', width: '30px', height: '30px', margin: '0 auto' }} />
@@ -47,10 +58,15 @@ const Movies = () => {
               </div>
             )
           })}
-
         </div>
       }
-
+      <div className={styles.pagination}>
+        {Array.from({ length: pageCount }, (v, index) => (
+          <button key={index + 1} onClick={() => handlePageChange(index + 1)} style={{ color: currentPage === index + 1 ? 'rgb(98, 155, 73)' : '#f3f3f3' }}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
