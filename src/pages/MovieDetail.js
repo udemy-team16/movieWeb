ca7c4be01cc70ed858cc41a677f5044976f5346c
annasyun <yunjbly2470@naver.com>
2023-06-30 17:39:22 +0900

    feat: 영화 즐겨찾기 추가

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -4,17 +4,49 @@ import { useParams } from "react-router-dom";
 import styles from "../style/movieDetail.module.css";
 
 const MovieDetail = () => {
+  const [like, setLike] = useState(false);
   const { id } = useParams();
   const { loading, responseData, errorMsg } = useFetch(
     "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
   );
   const [detailData, setDetailData] = useState();
+  const [likeArr, setLikeArr] = useState([]);
 
   useEffect(() => {
     setDetailData(
       responseData?.data.movies.filter((data) => data.id === parseInt(id))[0]
     );
-  }, [responseData?.data.movies, detailData]);
+  }, [responseData?.data.movies, detailData, id]);
+
+  // const handleLike = () => {
+  //   console.log(id);
+  //   setLikeArr([...likeArr, id]);
+  //   localStorage.setItem("favorite", likeArr);
+  //   setLike(!like);
+  // };
+
+  //
+
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setLikeArr(favorites);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(likeArr));
+  }, [likeArr]);
+
+  useEffect(() => {
+    setLike(likeArr.includes(id));
+  }, [id, likeArr]);
+
+  const handleLike = () => {
+    if (like) {
+      setLikeArr(likeArr.filter((itemId) => itemId !== id));
+    } else {
+      setLikeArr([...likeArr, id]);
+    }
+  };
 
   return (
     <section className={styles.section}>
@@ -24,6 +56,9 @@ const MovieDetail = () => {
         <p className={styles.year}>{detailData?.year}</p>
         <p className={styles.genres}>{detailData?.genres}</p>
         <p className={styles.rating}>{detailData?.rating}/10</p>
+        <button onClick={handleLike}>
+          {like ? "찜 해제하기🦄" : "찜 하기"}
+        </button>
       </div>
     </section>
   );
